33100d47b25f04232565a3e03c4bd178d0582038
awesomedevdude <aneeshmon@icloud.com>
2016-12-09 22:38:18 -0500

    New changes

---
+++ Selenium_Framework/src/main/java/Selenium/Selenium_Framework/App.java
@@ -0,0 +1,14 @@
+package Selenium.Selenium_Framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Another Message!");
+    }
+}
